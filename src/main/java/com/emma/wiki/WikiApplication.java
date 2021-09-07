bde748f1f0180ee7437dfa9cc1e8ceb6ebdda484
Emma <sh3956@columbia.edu>
2021-09-07 16:51:18 -0400

    Created: application.properties and logback-spring.xml

--- src/main/java/com/emma/wiki/WikiApplication.java
+++ src/main/java/com/emma/wiki/WikiApplication.java
@@ -1,14 +1,21 @@
 package com.emma.wiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 public class WikiApplication {
-
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("Started");
+        LOG.info("Address: \thttp://127.0.0.1:8880");
     }
+
 
 }
