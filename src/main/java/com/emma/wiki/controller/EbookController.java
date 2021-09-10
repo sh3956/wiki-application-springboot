049aa3f7613e3d59b52fa9cd7cdd7ef24f853205
Emma <sh3956@columbia.edu>
2021-09-10 15:42:49 -0400

    Add: Encapsulate request and response parameters

--- src/main/java/com/emma/wiki/controller/EbookController.java
+++ src/main/java/com/emma/wiki/controller/EbookController.java
@@ -1,7 +1,8 @@
 package com.emma.wiki.controller;
 
-import com.emma.wiki.domain.Ebook;
+import com.emma.wiki.req.EbookReq;
 import com.emma.wiki.resp.CommonResp;
+import com.emma.wiki.resp.EbookResp;
 import com.emma.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +18,9 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list(){
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq req){
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
