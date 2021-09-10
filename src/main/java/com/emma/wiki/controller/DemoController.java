a0eb1653553c5496608da38db002cbb0055cd8ee
Emma <sh3956@columbia.edu>
2021-09-09 21:57:24 -0400

    Add: query on demo table

---
+++ src/main/java/com/emma/wiki/controller/DemoController.java
@@ -0,0 +1,21 @@
+package com.emma.wiki.controller;
+import com.emma.wiki.domain.Demo;
+import com.emma.wiki.service.DemoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+    @Resource
+    private DemoService demoService;
+
+    @GetMapping("/list")
+    public List<Demo> list(){
+        return demoService.list();
+    }
+}
