03b1de0c744549b6ea28ca7cc3e07501565e4a70
Emma <sh3956@columbia.edu>
2021-09-10 14:51:27 -0400

    Add: Ebook table and generate its mapper file

--- doc/all.sql
+++ doc/all.sql
@@ -8,7 +8,7 @@ create table `test` (
 
 insert into `test` (id, name, password) values (1, 'test', 'password');
 
-
+###
 drop table if exists `demo`;
 create table `demo` (
                         `id` bigint not null comment 'id',
@@ -17,3 +17,30 @@ create table `demo` (
 ) engine=innodb default charset=utf8mb4 comment='test';
 
 insert into `demo` (id, name) values (1, 'test');
+
+###
+drop table if exists `ebook`;
+create table `ebook`
+(
+    `id`           bigint not null comment 'id',
+    `name`         varchar(50) comment 'name',
+    `category1_id` bigint comment 'category_1',
+    `category2_id` bigint comment 'category_2',
+    `description`  varchar(200) comment 'description',
+    `cover`        varchar(200) comment 'cover',
+    `doc_count`    int comment 'number of documents',
+    `view_count`   int comment 'number of views',
+    `vote_count`   int comment 'number of votes',
+    primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='ebook';
+
+insert into `ebook` (id, name, description) values (1, 'Spring Boot Beginning Tutorial',
+                    'Java Application, the best framework for enterprise application development')
+insert into `ebook` (id, name, description) values (2, 'Vue Beginning Tutorial',
+                    'Vue Application, the preferred framework for enterprise application development')
+insert into `ebook` (id, name, description) values (3, 'Python Beginning Tutorial',
+                    'Python Application, the best framework for enterprise application development')
+insert into `ebook` (id, name, description) values (4, 'Mysql Beginning Tutorial',
+                    'Mysql Application, the preferred framework for enterprise application development')
+insert into `ebook` (id, name, description) values (5, 'Oracle Beginning Tutorial',
+                    'Oracle Application, the best framework for enterprise application development')
