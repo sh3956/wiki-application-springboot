c95b1d31716b610dfe60a0ba42f5029b5ef60419
Emma <sh3956@columbia.edu>
2021-09-12 23:45:48 -0400

    Add: use Vue CLI to create web project

--- src/main/java/com/emma/wiki/service/EbookService.java
+++ src/main/java/com/emma/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.emma.wiki.domain.EbookExample;
 import com.emma.wiki.mapper.EbookMapper;
 import com.emma.wiki.req.EbookReq;
 import com.emma.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.emma.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,12 +22,14 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() +"%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook: ebookList){
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
-        return respList;
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook: ebookList){
+////            EbookResp ebookResp = new EbookResp();
+////            BeanUtils.copyProperties(ebook, ebookResp);
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(ebookResp);
+//        }
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+        return list;
     }
 }
