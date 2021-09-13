a117776d450db56f7ec5b3b430ec801920828214
Emma <sh3956@columbia.edu>
2021-09-13 17:15:14 -0400

    Feature: create the-header component

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,58 +1,32 @@
 <template>
-  <a-layout>
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-          theme="dark"
-          mode="horizontal"
-          v-model:selectedKeys="selectedKeys1"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
-     <router-view/>
+  <a-layout id="components-layout-demo-top-side-2">
+    <the-header></the-header>
+    <router-view/>
     <a-layout-footer style="text-align: center">
       Emma's Ebook
     </a-layout-footer>
   </a-layout>
 </template>
+
 <script lang="ts">
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons-vue';
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
+import TheHeader from '@/components/the-header.vue';
+
 export default defineComponent({
+  name: 'app',
   components: {
-    UserOutlined,
-    LaptopOutlined,
-    NotificationOutlined,
-  },
-  setup() {
-    return {
-      selectedKeys1: ref<string[]>(['2']),
-      selectedKeys2: ref<string[]>(['1']),
-      collapsed: ref<boolean>(false),
-      openKeys: ref<string[]>(['sub1']),
-    };
-  },
+    TheHeader,
+},
 });
+
 </script>
+
 <style>
 #components-layout-demo-top-side-2 .logo {
-  float: left;
   width: 120px;
   height: 31px;
-  margin: 16px 24px 16px 0;
-  background: rgba(255, 255, 255, 0.3);
-}
-
-.ant-row-rtl #components-layout-demo-top-side-2 .logo {
-  float: right;
-  margin: 16px 0 16px 24px;
-}
-
-.site-layout-background {
-  background: #fff;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px 28px 16px 0;
+  float: left;
 }
 </style>
