623e91e03d1f3271eecfb43468fff72a8f818e78
Emma <sh3956@columbia.edu>
2021-09-15 20:02:11 -0400

    Feature: axios interceptors, print request and response

--- web/src/main.ts
+++ web/src/main.ts
@@ -9,6 +9,20 @@ import axios from 'axios';
 
 axios.defaults.baseURL = process.env.VUE_APP_SERVER;
 
+axios.interceptors.request.use(function (config) {
+    console.log('Request：', config);
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+    console.log('Response：', response);
+    return response;
+}, error => {
+    console.log('Error：', error);
+    return Promise.reject(error);
+});
+
 const app = createApp(App);
 app.use(store).use(router).use(Antd).mount('#app');
 
