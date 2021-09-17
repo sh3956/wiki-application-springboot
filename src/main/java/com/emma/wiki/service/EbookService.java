032293d1a41e93433013f7d0deb606ce9aaf4103
Emma <sh3956@columbia.edu>
2021-09-16 20:47:29 -0400

    Add:integrate pagehelper

--- src/main/java/com/emma/wiki/service/EbookService.java
+++ src/main/java/com/emma/wiki/service/EbookService.java
@@ -6,6 +6,10 @@ import com.emma.wiki.mapper.EbookMapper;
 import com.emma.wiki.req.EbookReq;
 import com.emma.wiki.resp.EbookResp;
 import com.emma.wiki.util.CopyUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -14,25 +18,36 @@ import java.util.List;
 
 @Service
 public class EbookService {
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<EbookResp> list(EbookReq req){
+    public List<EbookResp> list(EbookReq req) {
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        if (!ObjectUtils.isEmpty(req.getName())){
-            criteria.andNameLike("%" + req.getName() +"%");
+        if (!ObjectUtils.isEmpty(req.getName())) {
+            criteria.andNameLike("%" + req.getName() + "%");
         }
+        PageHelper.startPage(1, 3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-//        List<EbookResp> respList = new ArrayList<>();
-//        for (Ebook ebook: ebookList){
-////            EbookResp ebookResp = new EbookResp();
-////            BeanUtils.copyProperties(ebook, ebookResp);
-//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
-//            respList.add(ebookResp);
-//        }
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("Number of rows：{}", pageInfo.getTotal());
+        LOG.info("Number of columns：{}", pageInfo.getPages());
+
+        // List<EbookResp> respList = new ArrayList<>();
+        // for (Ebook ebook : ebookList) {
+        //     // EbookResp ebookResp = new EbookResp();
+        //     // BeanUtils.copyProperties(ebook, ebookResp);
+        //     // 对象复制
+        //     EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+        //
+        //     respList.add(ebookResp);
+        // }
+
+        // 列表复制
         List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+
         return list;
     }
 }
